80f65aa5116ed7138bb7be42439901ec1264926c
Neranji Abeysekara <155614380+NeranjiAbeysekara@users.noreply.github.com>
2025-03-06 22:15:42 +0530

    routing the pages and components

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from "./pages/Login";
+import Signup from './pages/Signup'
+import Calender from './pages/Calender'
+import ChatPage from './pages/ChatPage'
+import Dashboard from './pages/Dashboard'
+import Notification from './pages/Notification'
+import  ProfilePage from './pages/ProfilePage'
+import Resources from './pages/Resources'
+import Tasks from './pages/Tasks'
+import Navbar from './components/Navbar'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/calender' element={<Calender/>}/>
+        <Route path='/chatpage' element={<ChatPage/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/notification' element={<Notification/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+        <Route path='/resources' element={<Resources/>}/>
+        <Route path='/tasks' element={<Tasks/>}/>
+      </Routes>
+      
+    </div>
   )
 }
 
